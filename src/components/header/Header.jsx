ef3ef8a2a7fe1fe436232b490166fee2fb815a14
bagherra99 <bagherra99@gmail.com>
2022-07-12 11:50:55 +0100

    sample commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import pp3 from '../../assets/pp3.jpg'
+import HeaderSocials from './HeaderSocials'
+
+    const header = () => {
+    return (
+        <header>
+            <div className='container header__container'>
+                <h5>Hello I'm</h5>
+                <h1>Idy Diagne</h1>
+                <h5 className='text-align'>Front-End Developer</h5>
+                <CTA />
+                <HeaderSocials/>
+
+                <div className='me'>
+                    <img src={pp3} alt='me'/>
+                </div>
+
+                <a href="#contact" className='scroll__down'>Scroll Down</a>
+
+            </div>
+        </header>
+    )
+    }
+
+export default header
